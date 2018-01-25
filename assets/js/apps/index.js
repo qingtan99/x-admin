d457609b02f49a54338c235c1d070e67c903ccb5
tanqing <123@qq.com>
2018-01-25 17:21:32 +0800

    使用require模块优化代码

---
+++ assets/js/apps/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2018/1/25.
+ */
+require.config(requireConfig);
+require(['jquery', 'bootstrap', 'metisMenu', 'slimscroll', 'layer', 'hplus', 'contabs'], function ($) {
+    'use strict';
+    var vm = {};
+
+});
