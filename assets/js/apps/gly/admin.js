52f706b9fb2488d6558f1c88fd74c6ab9ad49960
tanqing <qingtan99@126.com>
2018-01-28 22:05:42 +0800

    管理员页面添加弹窗

--- assets/js/apps/gly/admin.js
+++ assets/js/apps/gly/admin.js
@@ -2,9 +2,15 @@
  * Created by tanqing on 2018/1/25.
  */
 require.config(requireConfig);
-require(['jquery', 'bootstrap', 'content', 'iCheck'], function ($) {
+require(['jquery', 'bootstrap', 'content', 'iCheck', 'layer'], function ($) {
     'use strict';
     var vm = {};
+
+    var clickEvent = function() {
+        $("body")
+            .on("click", ".edit-btn", view.edit)
+            .on("click", ".dele-btn", view.dele)
+    }
 
     var view = {
         icheck: function() {
@@ -12,9 +18,32 @@ require(['jquery', 'bootstrap', 'content', 'iCheck'], function ($) {
                 checkboxClass: 'icheckbox_square-green',
                 radioClass: 'iradio_square-green',
             });
+        },
+
+        //编辑
+        edit: function(e) {
+            layer.open({
+                type: 2,
+                area: ['700px', '450px'],
+                fixed: false, //不固定
+                scrollbar: false,   //滚动条锁定
+                maxmin: true,
+                content: '123'
+            });
+        },
+
+        //删除
+        dele: function(e) {
+            layer.confirm('删除后将不可恢复！确认删除该管理员？', {
+                title: "提示信息",
+                btn: ['是的','取消'] //按钮
+            }, function(){
+                layer.msg('已删除', {icon: 1});
+            });
         }
     };
     vm.init = function() {
+        clickEvent();
         view.icheck();
     }
 
